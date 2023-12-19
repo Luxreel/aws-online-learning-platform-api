ed8328003b3d22de355f60f819f7e21a4f7f7fd2
Virginie Li <virginie.li@epita.fr>
2023-12-19 18:44:35 +0100

    Add CORS headers

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const port = 3001;
+const frontend_port = 3000;
+
+const allowedOrigins = [`http://localhost:${frontend_port}`, 'https://www.aws-online-learning-platform-app.cloudns.eu'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   res.send('Hello, Express!');
